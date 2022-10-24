f5d868d884480b77e3ada1a5d45185c21ff7c32c
Raychen1118 <106507149+Raychen1118@users.noreply.github.com>
2022-10-24 11:13:45 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oNv-g6hAKb8gmBLwURr4rMFbJ1mk',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'ljrDIgZP2ApiEVFzJspUjML6DAXMGgwjwrrQhqpujnw',
+      useTemplateId: 'NzDVn1CnN5hVPttXL8PM_FGnCrZegYQ7ZZlG25sahVM',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '05-21',
       festivals: [
@@ -49,7 +49,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'ljrDIgZP2ApiEVFzJspUjML6DAXMGgwjwrrQhqpujnw',
+  CALLBACK_TEMPLATE_ID: 'NzDVn1CnN5hVPttXL8PM_FGnCrZegYQ7ZZlG25sahVM',
 
   CALLBACK_USERS: [
     {
