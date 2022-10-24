f6c861a72b4cf55bf740fda5f7be29a35b5043b5
Raychen1118 <106507149+Raychen1118@users.noreply.github.com>
2022-10-24 14:55:00 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,9 +21,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '老婆',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oNv-g6pOhEBZeaBF-LHrs8mYJDJY',
+      id: 'oNv-g6hAKb8gmBLwURr4rMFbJ1mk,oNv-g6pOhEBZeaBF-LHrs8mYJDJY',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'oysZk1CbhJxyeGG3uW4-_mOW6RCrA0J_nQeJYkda0OE',
+      useTemplateId: 'MIsEkDLc3e9nYSIzTv_Hwf9e6IoQO0pjl4Lo3NgCyW0',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '05-21',
       festivals: [
@@ -47,7 +47,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'oysZk1CbhJxyeGG3uW4-_mOW6RCrA0J_nQeJYkda0OE',
+  CALLBACK_TEMPLATE_ID: 'MIsEkDLc3e9nYSIzTv_Hwf9e6IoQO0pjl4Lo3NgCyW0',
 
   CALLBACK_USERS: [
     {
